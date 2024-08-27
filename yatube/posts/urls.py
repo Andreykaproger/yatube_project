8ae6f87f6cd330df1fcab67e92b16a9f483c99d0
Dima <lleb41@mail.ru>
2024-08-27 19:10:46 +0300

    07_шаблоны Django пройдено

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
 
+app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name = 'start_page'),
+    path('group/<slug:slug>/', views.group_posts, name = 'groups'),
 ]
 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('главная страница')
+    template = 'posts/index.html'
+    title = 'это главная страница Yatube'
+    context = {
+        'title' : title,
+        'text' : 'Это главная страница'
+    }
+    return render(request, template, context)
 
 def group_posts(request, slug):
-    return HttpResponse(f'это страница с группами, отфильтрованными по {slug}')
+    template = 'posts/group_list.html'
+    context = {
+        'filter' : slug,
+        'text' : 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request,template, context)
 
 
