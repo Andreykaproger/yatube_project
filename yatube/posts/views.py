bb0f92619043104105eded6ddbb765bcff5b6b15
Dima <lleb41@mail.ru>
2024-08-30 12:32:30 +0300

    итоговый проект спринта готов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from .models import Post, Group
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')
     context = {
         'posts': posts,
     }
@@ -21,7 +21,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug = slug)
-    posts = Post.objects.filter(group = group).order_by('-pub_date')
+    posts = Post.objects.filter(group = group).order_by('-pub_date')[:10]
     context = {
         'group': group,
         'posts': posts,
